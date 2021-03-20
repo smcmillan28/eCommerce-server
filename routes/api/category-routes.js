2c7db4781f30abc542cac44ecf3b5fada9dd159c
Sammy McMillan <74073409+smcmillan28@users.noreply.github.com>
2021-03-20 12:36:43 -0400

    get and post routes working for category, product, tag

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,26 +3,71 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  try {
+    const catData = await Category.findAll({
+      include: [{ model: Product }],
+    });
+    res.status(200).json(catData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    const catData = await Category.findByPk(req.params.id, {
+      include: [{ model: Product }],
+    });
+
+    if (!catData) {
+      res.status(404).json({ message: 'No product found with that id!' });
+      return;
+    }
+
+    res.status(200).json(catData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  /* req.body should look like this...
+{
+  category_name: "Basketball",
+}
+*/
+  try {
+    const catData = await Category.create(req.body);
+    res.status(200).json(catData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update(req.body, {
+    where: {
+      id: req.params.id,
+    },
+  });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  let category = await Category.findOne({ where: { id: req.params.id } }).catch(e => {
+    console.log(e.message);
+  });
+  if (!tag) {
+    console.log("err");
+  }
+  category.destroy();
 });
 
 module.exports = router;
